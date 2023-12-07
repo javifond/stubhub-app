05c59cde76eaa0c09516ee5c9ae6603dfa3f7305
Javier Betancourth <javifond@gmail.com>
2023-12-07 20:05:48 +0100

    SH01 - Configuring Vite project to use port 3000 and css modules with SASS

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,19 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "src/styles/index.scss" as *;`,
+      },
+    },
+    modules: {
+      localsConvention: "camelCase",
+      generateScopedName: "[local]_[hash:base64:2]",
+    },
+  },
   plugins: [react()],
 });
