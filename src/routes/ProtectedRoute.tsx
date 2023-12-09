083b6915fe8f9b867c135273fb3f7d190eae4c6d
Javier Betancourth <javifond@gmail.com>
2023-12-09 16:09:26 +0100

    SH11 - Adding a protected route component for routes that needs in our case logged in users

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -3,11 +3,11 @@ import { Navigate } from "react-router-dom";
 import { APP_ROUTES } from "./routes.constants";
 
 type ProtectedRouteProps = {
-  user: number | null;
+  userId: number | null;
   children: ReactElement | ReactElement[];
 };
-export const ProtectedRoute = ({ user, children }: ProtectedRouteProps) => {
-  if (!user) {
+export const ProtectedRoute = ({ userId, children }: ProtectedRouteProps) => {
+  if (!userId) {
     return <Navigate to={APP_ROUTES.HOME} replace />;
   }
 
