e8b9d8eb90ca517e2d16bcec41473b5efb4f2d4c
Javier Betancourth <javifond@gmail.com>
2023-12-09 16:07:47 +0100

    SH11 - Modify the header to mimic a login / logout action and show the account link to the account page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,27 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useLoggedUser } from "../../hooks/useLoggedUser";
+import { APP_ROUTES } from "../../routes/routes.constants";
 import shLogo from "../../assets/img/logo.svg";
+
 import styles from "./Header.module.scss";
+import { getRandomUserId } from "../../utilities/getRandomUserId";
 
 const Header = () => {
+  const [logged, setLogged] = useState<boolean>(false);
+  const { setUserId } = useLoggedUser();
+
+  // HACK[JB]: This handler, mimics a mechanism
+  // where we will get a user that is logged in,
+  // requesting the data to an endpoint.
+  // It is only implemented for the home-test purpose.
+  const onLoginHandler = () => {
+    const randomUserId = getRandomUserId(1, 10);
+
+    setUserId(!logged ? randomUserId : null);
+    setLogged(!logged);
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.container}>
@@ -18,9 +37,19 @@ const Header = () => {
             Help
           </Link>
 
-          <Link to="/" title="Login" className={styles.linkItem}>
-            Login
-          </Link>
+          {logged && (
+            <Link
+              to={APP_ROUTES.ACCOUNT}
+              title="Account"
+              className={styles.linkItem}
+            >
+              Account
+            </Link>
+          )}
+
+          <button className={styles.button} onClick={onLoginHandler}>
+            {logged ? "Logout" : "Login"}
+          </button>
         </nav>
       </div>
     </header>
