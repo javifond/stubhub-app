dd769dff3e033c2aa5a6b21df77269d085d9ce65
Javier Betancourth <javifond@gmail.com>
2023-12-09 09:30:03 +0100

    SH08 - Implement the typeAhead search with the corresponding loaded data

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,15 +1,31 @@
+import { Typeahead } from "react-bootstrap-typeahead";
+import { Option } from "react-bootstrap-typeahead/types/types";
+import { useState } from "react";
+import { useFetchCategories } from "../../hooks/useFetchCategories";
+
 import styles from "./SearchBox.module.scss";
 
 const SearchBox = () => {
+  const { loading, error, categories } = useFetchCategories();
+  const [category, setCategory] = useState<Option[]>([]);
+
+  // TODO[JB]: Implement a better loading and error UX/UI,
+  // as if the data takes longer to load or there is an error,
+  // the component will not appear.
+  if (!categories || loading || error) return;
+
   return (
     <form action="#" method="get" className={styles.searchBox}>
-      <input
-        name="query"
-        autoComplete="off"
-        className={styles.input}
+      <Typeahead
+        id="searchBox"
+        labelKey="name"
+        onChange={(categorySelected) => setCategory(categorySelected)}
+        options={categories}
         placeholder="Search for an event, city, artist or team"
-        type="text"
+        selected={category}
+        className={styles.typeAhead}
       />
+
       <button type="submit" className={styles.button}>
         <i className="fa fa-search visible-phone"></i>
         <span className={styles.text}>Search</span>
