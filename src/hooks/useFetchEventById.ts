3491e27452ec2a18d31f502352f89d47a19dd747
Javier Betancourth <javifond@gmail.com>
2023-12-09 21:12:52 +0100

    SH12 - Use the api method to get the event by id instead

--- src/hooks/useFetchEventById.ts
+++ src/hooks/useFetchEventById.ts
@@ -1,42 +1,25 @@
 import { useState, useEffect } from "react";
 import { IEvent } from "../interfaces/IEvent";
-import { API_ENDPOINTS } from "../api/endpoints";
+import { getEventById } from "../api/methods/getEventById";
 
-export const useFetchEventById = (id: string | null) => {
+export const useFetchEventById = (id: number | string | null) => {
   const [event, setEvent] = useState<IEvent | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    const getEventById = async (): Promise<IEvent> => {
-      const event = await fetch(
-        `${API_ENDPOINTS.DOMAIN}/${API_ENDPOINTS.EVENTS}/${id}`
-      )
-        .then((res) => {
-          if (!res.ok) {
-            throw new Error(
-              `This is an HTTP error: The status is ${res.status}`
-            );
-          }
-
-          return res.json();
-        })
-        .catch((err) => {
-          if (err instanceof Error) console.log(err.message);
-          setError(err.message);
-          setEvent(null);
-        })
-        .finally(() => {
-          setLoading(false);
-        });
-
-      return event;
-    };
-
-    getEventById().then((event) => {
-      setEvent(event);
-      setError(null);
-    });
+    getEventById(id)
+      .then((event) => {
+        setEvent(event);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setEvent(null);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }, [id]);
 
   return { event, loading, error };
