3aa6e08013f6a42502ba54822fcae00b77933354
Javier Betancourth <javifond@gmail.com>
2023-12-09 12:52:44 +0100

    SH10 - Use the custom hooks to get the data of the available tickets and event name

--- src/pages/EventPage/EventPage.tsx
+++ src/pages/EventPage/EventPage.tsx
@@ -1,15 +1,25 @@
+import { useQuery } from "../../hooks/useQuery";
+import { useFetchTicketsByEvent } from "../../hooks/useFetchTicketsByEvent";
+import { useFetchEventById } from "../../hooks/useFetchEventById";
 import TicketsTable from "../../components/TicketsTable/TicketsTable";
-import { MOCK_TICKETS } from "../../mockData/tickets";
 
 import styles from "./EventPage.module.scss";
 
 const EventPage = () => {
+  const eventId = useQuery().get("eventId");
+  const { event } = useFetchEventById(eventId);
+  const { tickets, loading, error } = useFetchTicketsByEvent(eventId);
+
+  // TODO[JB]: Implement a better loading and error UX/UI,
+  // as if the data takes longer to load or there is an error,
+  // the component will not appear.
+  if (!tickets || loading || error) return;
+
   return (
     <main>
       <div className={styles.container}>
-        {/* TODO[JB]: The name of the event should be passed as prop or with the router TBD */}
-        <h2 className={styles.title}>Event Name</h2>
-        <TicketsTable tickets={MOCK_TICKETS} />
+        {event && <h2 className={styles.title}>{event.title}</h2>}
+        <TicketsTable tickets={tickets} />
       </div>
     </main>
   );
