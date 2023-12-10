2b8a973dbe5dcd36e794fbf4ed877c9dd8aa82ac
Javier Betancourth <javifond@gmail.com>
2023-12-10 14:29:44 +0100

    SH15 - Dispatch actions on click

--- src/components/TicketActions/TicketActions.tsx
+++ src/components/TicketActions/TicketActions.tsx
@@ -1,22 +1,44 @@
+import { ITicketListAction } from "../../interfaces/ITicketListAction";
+import { TicketListReducerActions } from "../../reducers/TicketListReducer/TicketListReducer.actions";
 import styles from "./TicketActions.module.scss";
 
 type TicketActionsProps = {
+  eventId: number;
   ticketStatus: boolean;
   setTicketStatus: React.Dispatch<React.SetStateAction<boolean>>;
+  dispatch: React.Dispatch<ITicketListAction>;
 };
 
 const TicketActions = ({
+  eventId,
   ticketStatus,
   setTicketStatus,
+  dispatch,
 }: TicketActionsProps) => {
   return (
     <div className={styles.ticketItem}>
-      <button className={styles.button}>
+      <button
+        className={styles.button}
+        onClick={() =>
+          dispatch({
+            type: TicketListReducerActions.DUPLICATE,
+            payload: { eventId },
+          })
+        }
+      >
         <i className="fa fa-files-o"></i>
         <span className={styles.iconText}>Duplicate</span>
       </button>
 
-      <button className={styles.button}>
+      <button
+        className={styles.button}
+        onClick={() =>
+          dispatch({
+            type: TicketListReducerActions.DELETE,
+            payload: { eventId },
+          })
+        }
+      >
         <i className="fa fa-trash"></i>
         <span className={styles.iconText}>Remove</span>
       </button>
