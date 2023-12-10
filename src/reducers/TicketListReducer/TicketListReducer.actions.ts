e1e54924e02d6278b8e3f92b9ddecc267cd8ce96
Javier Betancourth <javifond@gmail.com>
2023-12-10 14:27:28 +0100

    SH15 - Reducer action definition

---
+++ src/reducers/TicketListReducer/TicketListReducer.actions.ts
@@ -0,0 +1,8 @@
+/**
+ * Lists all the possible actions of the reducer.
+ */
+export enum TicketListReducerActions {
+  UPDATE_LIST = "UPDATE_LIST",
+  DUPLICATE = "DUPLICATE",
+  DELETE = "DELETE",
+}
